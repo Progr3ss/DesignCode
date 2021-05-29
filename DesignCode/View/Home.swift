da77cf2bc2e128403f340f79721a740737f557b0
martin <mchibwe01@gmail.com>
2021-05-29 00:06:28 -0700

    Binding and screen size

--- DesignCode/View/Home.swift
+++ DesignCode/View/Home.swift
@@ -14,37 +14,20 @@ struct Home: View {
         ZStack {
             Color(#colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1))
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            
-            VStack {
-                HStack {
-                    Text("Watching")
-                        .font(.system(size: 28, weight: .bold))
-                    Spacer()
-                    Button(action: {self.showProfile.toggle()}) {
-                        Image("Avatar")
-                            .renderingMode(.original)
-                            .resizable()
-                            .frame(width: 36, height: 36)
-                            .clipShape(Circle())
-                            
-                    }
-                }
-                .padding(.horizontal)
-                .padding(.top,30)
-                Spacer()
-            }
-            .padding(.top,44)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
-            .scaleEffect(showProfile ? 0.9 : 1)
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
-            .offset(y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10.0, y: 0, z: 0))
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            HomeView(showProfile: $showProfile)
+                .padding(.top,44)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+                .scaleEffect(showProfile ? 0.9 : 1)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+                .offset(y: showProfile ? -450 : 0)
+                .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             MenuView()
                 .background(Color.black.opacity(0.0001))
-                .offset(y: showProfile ? 0 : 1200)
+                .offset(y: showProfile ? 0 : screen.height)
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
                 .onTapGesture {
                     self.showProfile.toggle()
@@ -55,6 +38,10 @@ struct Home: View {
                         self.viewState = value.translation
                     }
                         .onEnded{value in
+                            self.viewState = .zero
+                            if self.viewState.height > 50 {
+                                self.showProfile = false
+                            }
                             self.viewState = .zero
                         }
                 )
@@ -68,3 +55,18 @@ struct Home_Previews: PreviewProvider {
         Home()
     }
 }
+
+struct AvatarView: View {
+    @Binding var showProfile: Bool
+    var body: some View {
+        Button(action: {self.showProfile.toggle()}) {
+            Image("Avatar")
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 36, height: 36)
+                .clipShape(Circle())
+        }
+    }
+}
+
+let screen = UIScreen.main.bounds
