ab296f133078782c94fec15fe2e1d3c7a953c31f
martin <mchibwe01@gmail.com>
2021-05-29 15:03:52 -0700

    data and image literal

--- DesignCode/View/HomeView.swift
+++ DesignCode/View/HomeView.swift
@@ -22,8 +22,8 @@ struct HomeView: View {
             .padding(.top,30)
             ScrollView(.horizontal, showsIndicators: false){
                 HStack {
-                    ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                        SectionView()
+                    ForEach(sectionData) { item in
+                        SectionView(section: item)
                     }
                 }
                 .padding(30)
@@ -42,19 +42,21 @@ struct HomeView_Previews: PreviewProvider {
 }
 
 struct SectionView: View {
+    var section: Section
+    
     var body: some View {
         VStack {
             HStack(alignment:.top){
-                Text("Prototype designs in SwiftUI")
+                Text(section.title)
                     .font(.system(size: 24, weight: .bold))
                     .frame(width: 160, alignment: .leading)
                     .foregroundColor(Color.white)
                 Spacer()
-                Image("Logo1")
+                Image(section.logo)
             }
-            Text("18 Sections".uppercased())
+            Text(section.text.uppercased())
                 .frame(maxWidth: .infinity,alignment: .leading)
-            Image("Card1")
+            section.image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 210)
@@ -63,8 +65,25 @@ struct SectionView: View {
         .padding(.top, 20)
         .padding(.horizontal, 20)
         .frame(width: 275, height: 275)
-        .background(Color("card1"))
+        .background(section.color)
         .cornerRadius(30)
-        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
     }
 }
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+let sectionData = [
+    Section(title: "Prototype Designs in SwiftUI", text: "189 Section", logo: "logo2", image: Image(uiImage: #imageLiteral(resourceName: "Card4")), color: Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))),
+    Section(title: "Prototype Designs in SwiftUI", text: "189 Section", logo: "logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card6")), color: Color(#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1))),
+    Section(title: "Prototype Designs in SwiftUI", text: "189 Section", logo: "logo2", image: Image(uiImage: #imageLiteral(resourceName: "Card6")), color: Color(#colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1))),
+    Section(title: "Prototype Designs in SwiftUI", text: "189 Section", logo: "logo2", image: Image(uiImage: #imageLiteral(resourceName: "Card1")), color: Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))),
+    Section(title: "Prototype Designs in SwiftUI", text: "189 Section", logo: "logo2", image: Image(uiImage: #imageLiteral(resourceName: "Logo2")), color: Color(#colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)))
+    
+    
+]
